4b460ee5edeca1af2177aada022d2c6e93aba845
Rachman Aldiansyah <rachman@pop-os.localdomain>
2023-10-28 23:27:21 +0700

    create SearchBar.tsx && SearchManufacturer.tsx for Car Categories

--- components/index.ts
+++ components/index.ts
@@ -2,10 +2,16 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
+import CustomFilter from "./CustomFilter";
 
 export {
     Hero,
     CustomButton,
     Navbar,
-    Footer
+    Footer,
+    SearchBar,
+    SearchManufacturer,
+    CustomFilter
 }
